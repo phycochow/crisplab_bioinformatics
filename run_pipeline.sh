12029ee8646beda35d28f0b82e3c865718e4a262
Chris Siu Yeung Chow <132418828+phycochow@users.noreply.github.com>
2023-05-06 16:49:20 +1000

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -10,13 +10,13 @@
 
 # Set variables 
 path_to_sample_list=/scratch/project/crisp008/chris/NGS_project/inputs/samples.txt
-path_to_qc=/home/s4669612/gitrepos/crisplab_wgs/01-qc_sbatch.sh
-path_to_trim=/home/s4669612/gitrepos/crisplab_wgs/01-trim_galore_gz_sbatch.sh
-path_to_bowtie=/home/s4669612/gitrepos/crisplab_wgs/02-bowtie2_sbatch.sh
+path_to_qc_job=/home/s4669612/gitrepos/crisplab_wgs/01-qc_sbatch.sh
+path_to_trim_job=/home/s4669612/gitrepos/crisplab_wgs/01-trim_galore_gz_sbatch.sh
+path_to_bowtie_job=/home/s4669612/gitrepos/crisplab_wgs/02-bowtie2_sbatch.sh
 path_to_reference=/scratch/project/crisp008/chris/NGS_project/inputs/sorghum/Sbicolor_454_v3.0.1_vectors
 
 # Submit the first job and get the job ID
-fastqc_job=$(sbatch --parsable "$path_to_qc" "$path_to_sample_list" 2:00:00 8 a_crisp)
+fastqc_job=$(sbatch --parsable "$path_to_qc_job" "$path_to_sample_list" 2:00:00 8 a_crisp)
 
 # Run MultiQC after FastQC is done
 if [[ $SLURM_ARRAY_TASK_ID == 2 ]]; then
@@ -27,8 +27,8 @@ if [[ $SLURM_ARRAY_TASK_ID == 2 ]]; then
 fi
 
 # Submit the second job and set its dependency on the first job
-trim_galore_job=$(sbatch --parsable --dependency=afterok:$fastqc_job "$path_to_trim" "$path_to_sample_list" 20:00:00 16 a_crisp)
+trim_galore_job=$(sbatch --parsable --dependency=afterok:$fastqc_job "$path_to_trim_job" "$path_to_sample_list" 20:00:00 16 a_crisp)
 
 # Submit the third job and set its dependency on the second job
-bowtie2_job=$(sbatch --parsable --dependency=afterok:$trim_galore_job "$path_to_trim" "$path_to_sample_list" trimmed 6 "$path_to_reference" 10 18:00:00 40 a_crisp)
+bowtie2_job=$(sbatch --parsable --dependency=afterok:$trim_galore_job "$path_to_bowtie_job" "$path_to_sample_list" trimmed 6 "$path_to_reference" 10 18:00:00 40 a_crisp)
 
