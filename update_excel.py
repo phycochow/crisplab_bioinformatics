5199a1752703045b876f2ac9dcdf1813c7c57441
Chris Siu Yeung Chow <132418828+phycochow@users.noreply.github.com>
2023-05-07 19:32:17 +1000

    Update update_excel.py

--- update_excel.py
+++ update_excel.py
@@ -3,7 +3,7 @@ import argparse
 import pandas as pd
 
 
-def oooweeeeeeeeeeee():
+def count_mapped_reads():
     # parse the command-line arguments
     parser = argparse.ArgumentParser(description="Count mapped reads in a BAM file in a specific region.")
     parser.add_argument("chromosome", type=str, help="Chromosome name (e.g. chr1)")
@@ -21,12 +21,22 @@ def oooweeeeeeeeeeee():
     for read in bamfile.fetch(args.chromosome, start_index, end_index):
         if not read.is_unmapped:
             mapped_reads += 1
+    
+    # Calculate read coverage by getting the reference sequence length for the specified chromosome
+    total_bases = bamfile.get_reference_length(args.chromosome)
+    covered_bases = 0
+    for pileupcolumn in bamfile.pileup(args.chromosome):
+        if pileupcolumn.n > 0:
+            covered_bases += 1
+
+    # Calculate the coverage percentage
+    coverage_percentage = (covered_bases / total_bases) * 100
 
     df = pd.read_csv(args.output_file)
 
     # add row [chromosome, start, end, mapped reads]
-#     df.loc[len(df)] = [args.chromosome, args.start_pos, args.end_pos, mapped_reads]
-    df.loc[len(df)] = [args.chromosome, start_index, end_index, mapped_reads]
+#     df.loc[len(df)] = [args.chromosome, args.start_pos, args.end_pos, mapped_reads, coverage_percentage]
+    df.loc[len(df)] = [args.chromosome, start_index, end_index, mapped_reads, coverage_percentage]
 
     # write the output to an Excel file
     df.to_csv(args.output_file, index=False)
@@ -34,7 +44,7 @@ def oooweeeeeeeeeeee():
 
 if __name__ == "__main__":
     """This runs the python file as a script and prevents the terminal from treating it as a module."""
-    oooweeeeeeeeeeee()  # Updates a preexisting csv file
+    count_mapped_reads()  # Updates a preexisting csv file
     """terminal input example: my_script.py chr1 my_bam_file.bam output.csv"""
     """my_script.py Cloned_ykaf_nptII my_bam_file.bam output.csv"""
     """Specifically Cloned_ykaf_nptII and P2_P_Contig_1__zCas9"""
