635f8a16cc5ccb7850da798514691083f08a8571
Chris Siu Yeung Chow <132418828+phycochow@users.noreply.github.com>
2023-05-07 19:37:38 +1000

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -15,6 +15,7 @@ path_to_bowtie_script=/home/s4669612/gitrepos/crisplab_wgs/02-bowtie2_sbatch.sh
 path_to_reference=/scratch/project/crisp008/chris/NGS_project/inputs/sorghum/Sbicolor_454_v3.0.1_vectors
 
 # Submit the first job and get the job ID
+cd /scratch/project/crisp008/chris/NGS_project/processing
 fastqc_job=$(sbatch --parsable "$path_to_qc_script" "$path_to_sample_list" 2:00:00 8 a_crisp)
 
 # Run MultiQC after FastQC is done
@@ -26,9 +27,11 @@ if [[ $SLURM_ARRAY_TASK_ID == 2 ]]; then
 fi
 
 # Submit the second job and set its dependency on the first job
+cd /scratch/project/crisp008/chris/NGS_project/processing
 trim_galore_job=$(sbatch --parsable --dependency=afterok:$fastqc_job "$path_to_trim_script" "$path_to_sample_list" 20:00:00 16 a_crisp)
 
 # Submit the third job and set its dependency on the second job
+cd /scratch/project/crisp008/chris/NGS_project/processing
 bowtie2_job=$(sbatch --parsable --dependency=afterok:$trim_galore_job "$path_to_bowtie_script" "$path_to_sample_list" trimmed 6 "$path_to_reference" 10 18:00:00 40 a_crisp)
 
 # Submit the fourth job and set its dependency on the third job
