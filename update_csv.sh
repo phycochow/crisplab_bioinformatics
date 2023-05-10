50c8f5db4d35ac7cb903bf8785f8517c6cfa731a
Chris Siu Yeung Chow <132418828+phycochow@users.noreply.github.com>
2023-05-11 09:42:48 +1000

    Create update_csv.sh

---
+++ update_csv.sh
@@ -0,0 +1,11 @@
+
+
+# # Store the ouputs: Loop over each vector in the vector library - to be improved (ask pete about coverage, read counts and other features logs)
+# vector_list=("P2_P_Contig_1__zCas9" "Cloned_ykaf_nptII")
+# for vector in "${vector_list[@]}"; do
+#   for file in analysis/trimmed_align_bowtie2/*.bam; do
+#     python /home/s4669612/gitrepos/crisplab_wgs/update_excel.py "$vector" "$file" ../outputs/output.csv ;
+#   done
+# done
+# conda deactivate
+# done
