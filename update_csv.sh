5c0f2ab7e10a3d4de82bf0e1e75bde05d6880556
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2023-05-13 22:05:01 +1000

    Update update_csv.sh

--- update_csv.sh
+++ update_csv.sh
@@ -1,11 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=feature_extraction
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=2:00:00
+#SBATCH --partition=general
+#SBATCH --requeue
 
+######### Setup ################
+# Set file paths
+path_to_feature_script=/home/s4669612/gitrepos/crisplab_wgs/feature_extraction.py
+path_to_output_csv=/scratch/project/crisp008/chris/NGS_project/outputs/output.csv
 
-# # Store the ouputs: Loop over each vector in the vector library - to be improved (ask pete about coverage, read counts and other features logs)
-# vector_list=("P2_P_Contig_1__zCas9" "Cloned_ykaf_nptII")
-# for vector in "${vector_list[@]}"; do
-#   for file in analysis/trimmed_align_bowtie2/*.bam; do
-#     python /home/s4669612/gitrepos/crisplab_wgs/update_excel.py "$vector" "$file" ../outputs/output.csv ;
-#   done
-# done
-# conda deactivate
-# done
+# Activate conda environment
+source /home/s4669612/miniconda3/bin/activate py3.7
+
+# Define the vector list
+vector_list=("P2_P_Contig_1__zCas9" "Cloned_ykaf_nptII")
+
+# Loop over each vector in the vector list
+for vector in "${vector_list[@]}"; do
+
+  # Loop over each BAM file in the trimmed_align_bowtie2 directory
+  for file in analysis/trimmed_align_bowtie2/*.bam; do
+  
+    # Run the feature extraction script
+    python "$path_to_feature_script" "$vector" "$file" "$path_to_output_csv"
+  done
+done
+
+# Deactivate conda environment
+conda deactivate
