d7d401807f0e1b0190b092d813f0dccabf323a08
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2023-05-14 01:48:35 +1000

    Update 00-parameter_sweep.sh

--- 00-parameter_sweep.sh
+++ 00-parameter_sweep.sh
@@ -7,7 +7,7 @@
 #SBATCH --requeue
 
 usage="USAGE:
-bash parameter_sweep.sh <list_of_percentages> <fastq_directory>"
+bash parameter_sweep.sh <list_of_percentages> <id>"
 
 ######### Setup ################
 # Check if the number of arguments is correct
@@ -20,17 +20,24 @@ fi
 # Get the list of percentages from the command-line argument
 percentages=("${@:1:$#-1}")
 
-# Get the fastq directory from the command-line argument
-fastq_directory="${@: -1}"
+# Get the id from the command-line argument e.g. 1, 2, 3... for sbatch
+id="${@: -1}"
+
+# Store current working directory
+current_dir=$(pwd)
 
 # Set up file paths
 path_to_pipeline_script=/home/s4669612/gitrepos/crisplab_wgs/00-pipeline.sh
-path_to_outputs=output.csv
+fastq_directory="inputs/reads$id"
+working_directory="processing$id"
+
+# In case this script is run directly, make reads folder
+mkdir -p "$fastq_directory" "$working_directory"
 
 # Loop over each percentage and process the files
 for percentage in "${percentages[@]}"; do
   # Copy the raw_reads into the temporary read folder to be processed
-  for file in "$fastq_directory"/*; do
+  for file in inputs/raw_reads_template/; do
     cp "$file" "$fastq_directory"
   done
   
@@ -45,6 +52,8 @@ for percentage in "${percentages[@]}"; do
   done
   
   # Run and wait for the pipeline job to complete
+  cd $working_directory
   run_pipeline_job=$(sbatch --parsable "$path_to_pipeline_script" "$fastq_directory")
+  cd $current_dir
 done
  
