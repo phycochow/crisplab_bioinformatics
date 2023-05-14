1a4577b17a269f809faf61eefef1576ab0b462a2
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2023-05-14 15:28:57 +1000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=main
-#SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=8G
-#SBATCH --time=2:00:00
+#SBATCH --job-name=Chris_NGS
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=96
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --array=1-9600%96  # Run in batches of 96, up to a total of 9600 times
+#SBATCH --time=200:00:00
 #SBATCH --partition=general
-#SBATCH --requeue
-#SBATCH --array=1-6240%96  # Run in batches of 40, up to a total of 6000 times
+#SBATCH --account=a_crisp
 
 # Path to the main script
 path_to_parameter_sweep_script=/home/s4669612/gitrepos/crisplab_wgs/00-parameter_sweep.sh
@@ -14,7 +16,7 @@ path_to_parameter_sweep_script=/home/s4669612/gitrepos/crisplab_wgs/00-parameter
 percentages=($(seq 0.0009 0.01 0.9909))
 
 # Set the total number of job arrays
-total_arrays=6240
+total_arrays=9600
 
 # Path to the script for each job array
 path_to_job_script=/path/to/your/job_script.sh
