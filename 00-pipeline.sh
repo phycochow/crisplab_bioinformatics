7606c681093e5932f064f6a01d45c36c5f552559
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2023-05-14 18:14:36 +1000

    Update 00-pipeline.sh

--- 00-pipeline.sh
+++ 00-pipeline.sh
@@ -7,7 +7,7 @@
 #SBATCH --requeue
 
 usage="USAGE:
-bash 00-pipeline.sh <fastq_directory>"
+bash 00-pipeline.sh <fastq_directory> <processing_directory>"
 
 ######### Setup ################
 # Check if the number of arguments is correct
@@ -19,16 +19,18 @@ fi
 
 # Get the fastq directory from the command-line argument
 fastq_directory=$1
+processing_directory=$2
 
 # Set file paths 
 path_to_sample_list=/scratch/project/crisp008/chris/NGS_project/inputs/samples.txt
+path_to_reference=/scratch/project/crisp008/chris/NGS_project/inputs/sorghum/Sbicolor_454_v3.0.1_vectors
 path_to_qc_script=/home/s4669612/gitrepos/crisplab_wgs/01-qc_sbatch.sh
 path_to_trim_script=/home/s4669612/gitrepos/crisplab_wgs/01-trim_galore_gz_sbatch.sh
 path_to_bowtie_script=/home/s4669612/gitrepos/crisplab_wgs/02-bowtie2_sbatch.sh
-path_to_reference=/scratch/project/crisp008/chris/NGS_project/inputs/sorghum/Sbicolor_454_v3.0.1_vectors
+path_to_extract_bam_features_script=/home/s4669612/gitrepos/crisplab_wgs/05-extract_bam_features.sh
 
 # Submit the first job and get the job ID
-cd /scratch/project/crisp008/chris/NGS_project/processing
+cd processing_directory
 fastqc_job=$(sbatch --parsable "$path_to_qc_script" "$path_to_sample_list" 2:00:00 8 a_crisp, "$fastq_directory")
 
 # Run MultiQC after FastQC is done
@@ -40,12 +42,28 @@ if [[ $SLURM_ARRAY_TASK_ID == 2 ]]; then
 fi
 
 # Submit the second job and set its dependency on the first job
-cd /scratch/project/crisp008/chris/NGS_project/processing
+cd processing_directory
 trim_galore_job=$(sbatch --parsable --dependency=afterok:$fastqc_job "$path_to_trim_script" "$path_to_sample_list" 20:00:00 16 a_crisp)
 
+
+# Exttract trim features after trim is done
+if [[ $SLURM_ARRAY_TASK_ID == 3 ]]; then
+  cd logs
+  cdn
+  echo -e "Sample\tTotal_sequences_analysed\tPERCENT_READS_WITH_ADAPTERS_R1\tPERCENT_READS_WITH_ADAPTERS_R2\tPERCENT_BP_TRIMMED_R1\tPERCENT_BP_TRIMMED_R2" > ../total_reads_summary.tsv
+fi
+
 # Submit the third job and set its dependency on the second job
-cd /scratch/project/crisp008/chris/NGS_project/processing
+cd processing_directory
 bowtie2_job=$(sbatch --parsable --dependency=afterok:$trim_galore_job "$path_to_bowtie_script" "$path_to_sample_list" trimmed 6 "$path_to_reference" 10 18:00:00 40 a_crisp)
+
+
+# Feature extraction section, make sure the file removal is in the last script here   
+# Run MultiQC after FastQC is done
+
+
+
+extract_bam_features_job=$(sbatch --parsable --dependency=afterok:$bowtie2_job "$path_to_extract_bam_features_script" "$fastq_directory")
 
 # Submit the fourth job and set its dependency on the third job
 # path_to_trimmed_bowtie=/scratch/project/crisp008/chris/NGS_project/test3/analysis/trimmed_align_bowtie2
