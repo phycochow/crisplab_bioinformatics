7fbb061fe73f453a9727a33027bcdc982d19971a
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2023-05-14 15:05:34 +1000

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,27 +10,22 @@
 # Path to the main script
 path_to_parameter_sweep_script=/home/s4669612/gitrepos/crisplab_wgs/00-parameter_sweep.sh
 
-# Construct the list of percentages
-percentages=($(seq 0.0009 0.01 0.9909))
-sbatch --array=1-40%40 parameter_sweep.sh "${percentages[@]}" "$batch"
-
-
-
-
-# Define the total number of iterations
-total_iterations=6000
-
-# Construct the list of percentages
+# Construct the list of percentages - constant throughout the script
 percentages=($(seq 0.0009 0.01 0.9909))
 
-# Loop over each batch
-for (( batch=1; batch <= total_iterations; batch+=40 )); do
+# Set the total number of job arrays
+total_arrays=6240
 
-  # Calculate the start and end index for the current batch
-  start_index=$(( batch ))
-  end_index=$(( batch + 39 ))
+# Path to the script for each job array
+path_to_job_script=/path/to/your/job_script.sh
 
-  # Submit the job to run the parameter sweep script with the current batch of percentages
-  sbatch --array=1-40%40 parameter_sweep.sh "${percentages[@]}" "$batch"
-
+# Loop over the number of job arrays
+for ((id=1; id<=total_arrays; id++)); do
+  # Submit each job array with dependency on the previous job array
+  if [[ $id -eq 1 ]]; then
+    sbatch --array=1-96%96 "$path_to_parameter_sweep_script" "${percentages[@]}" "$id"
+  else
+    previous_array=$((id-1))
+    sbatch --array=1-96%96 --dependency=afterok:$previous_array "$path_to_parameter_sweep_script" "${percentages[@]}" "$id"
+  fi
 done
