e339f52c66a47f1be5c8aaf3a46be0c9a80cde40
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2023-05-15 21:37:46 +1000

    Update 00-parameter_sweep.sh

--- 00-parameter_sweep.sh
+++ 00-parameter_sweep.sh
@@ -31,6 +31,7 @@ working_directory=$(pwd)
 
 # Set up file paths
 path_to_pipeline_script=/home/s4669612/gitrepos/crisplab_wgs/00-pipeline.sh
+path_to_subsampling_script=/home/s4669612/gitrepos/crisplab_wgs/00-subsample_fastqgz.sh
 
 #################################### Run ########################################
 # Loop over each percentage and process the files
@@ -39,29 +40,17 @@ for percentage in "${percentages[@]}"; do
   # Go/return to parent directory of inputs outputs and processing  
   cd "$working_directory"
   
-  # Store and create read and processing directories with id and percentage
+  # Store and create directories with id number & respective percentage
   fastq_directory="$working_directory"/inputs/reads"$id"_"$percentage"
   processing_directory="$working_directory"/processing"$id"_"$percentage"
   mkdir "$fastq_directory" "$processing_directory"
   
-  # Copy the raw_reads into the temporary read folder to be processed
-  for file in ../raw_reads_template/*; do
-    cp "$file" "$fastq_directory"
-  done
-  
-  # Subsample and uncompress the fastq files
-  for file in "$fastq_directory"/*; do
-    /home/s4669612/software/seqtk/seqtk sample -s100 "$file" "$percentage" > "${file%.fastq.gz}.fastq"
-  done
-      
-  # Delete all compressed files in the new directory
-  for file in "$fastq_directory"/*.fastq.gz; do
-    rm "$file"
-  done
-  
+  # Create subsampled fastq files
+  subsampling_job=(sbatch path_to_subsampling_script $fastq_directory" "$percentage")
+    
   # Go into the processing directory, submit a sbatch for the pipeline job to be completed, then obtain the data
   cd "$processing_directory"
   mkdir analysis logs
-  run_pipeline_job=$(sbatch "$path_to_pipeline_script" "$fastq_directory" "$processing_directory")
+  run_pipeline_job=$(sbatch --dependency=afterok:$subsampling_job "$path_to_pipeline_script" "$fastq_directory" "$processing_directory")
 done
  
