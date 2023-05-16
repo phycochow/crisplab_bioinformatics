7ee5519ab10443dae66667dbc69c61c6640768c9
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2023-05-16 20:04:27 +1000

    Update 00-pipeline.sh

--- 00-pipeline.sh
+++ 00-pipeline.sh
@@ -26,7 +26,7 @@ percentage=$3
 inputs_directory="$fastq_directory"/..
 
 # Set file paths 
-path_to_sample_list=./"$inputs_directory"/samples.txt
+path_to_sample_list=/"$inputs_directory"/samples.txt
 path_to_reference="$inputs_directory"/sorghum/Sbicolor_454_v3.0.1_vectors
 path_to_qc_script=/home/s4669612/gitrepos/crisplab_wgs/01-qc_sbatch.sh
 path_to_trim_script=/home/s4669612/gitrepos/crisplab_wgs/01-trim_galore_gz_sbatch.sh
