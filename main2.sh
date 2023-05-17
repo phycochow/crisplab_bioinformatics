cfee950f177fb02c6e4e8adf93390ae704b5159e
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2023-05-17 23:47:41 +1000

    Update main2.sh

--- main2.sh
+++ main2.sh
@@ -9,12 +9,18 @@
 #SBATCH --account=a_crisp
 
 #################################### Setup ########################################
+usage="USAGE:
+bash 00-main.sh <working_directory> <id>"
+
+working_directory=$1
+
 # Path to the main script
 path_to_parameter_sweep_script=/home/s4669612/gitrepos/crisplab_wgs/00-parameter_sweep.sh
+path_to_pipeline_script=/home/s4669612/gitrepos/crisplab_wgs/00-pipeline.sh
+path_to_subsampling_script=/home/s4669612/gitrepos/crisplab_wgs/00-subsample_fastqgz.sh
 
 # Construct the list of percentages - constant throughout the script
 percentages=($(seq 0.01 0.01 0.96))
-
 
 # Function to check if all jobs in a batch have completed
 check_batch_completion() {
@@ -32,29 +38,45 @@ check_batch_completion() {
 #################################### Run ########################################
 # Specify the total number of jobs and the batch size
 total_jobs=400
-batch_size=4
+batch_size=
 
-# Loop through the job array
-for ((i=1; i<=total_jobs; i+=batch_size)); do
-    batch_jobs=()
 
-    # Submit jobs for the current batch
-    for ((j=i; j<i+batch_size; j++)); do
-        if [ $j -le $total_jobs ]; then
-            job_id=$(sbatch --dependency=afterok:"$dependency" "$path_to_parameter_sweep_script" "${percentages[@]}" "$i" $j | awk '{print $4}')
-            batch_jobs+=("$job_id")
-        fi
+for percentage in "${percentages[@]}"; do
+
+    # Loop through the job array
+    for ((i=1; i<=total_jobs; i+=batch_size)); do
+        batch_jobs=()
+
+        # Submit jobs for the current batch
+        for ((j=i; j<i+batch_size; j++)); do
+            if [ $j -le $total_jobs ]; then
+                # Go/return to parent directory of inputs outputs and processing  
+                 cd "$working_directory"
+  
+                # Store and create directories with id number & respective percentage
+                fastq_directory="$working_directory"/inputs/reads"$j"_"$percentage"
+                processing_directory="$working_directory"/processing"$j"_"$percentage"
+                mkdir "$fastq_directory" "$processing_directory"   
+                subsampling_job=$(sbatch --parsable "$path_to_subsampling_script" "$fastq_directory" "$percentage")
+
+
+                mkdir "$processing_directory"/analysis "$processing_directory"/logs
+                # Stores job ID with --parsable
+                run_pipeline_job=$(sbatch --parsable --dependency=afterok:$subsampling_job "$path_to_pipeline_script" "$fastq_directory" "$processing_directory" "$percentage")
+                batch_jobs+=("$run_pipeline_job")
+
+            fi
+        done
+
+        # Wait for the current batch to complete
+        while true; do
+            check_batch_completion
+            if [ $? -eq 0 ]; then
+                break
+            fi
+            sleep 10
+        done
+
+        # Set the dependency for the next batch
+        dependency=$(IFS:; echo "${batch_jobs[*]}")
     done
-
-    # Wait for the current batch to complete
-    while true; do
-        check_batch_completion
-        if [ $? -eq 0 ]; then
-            break
-        fi
-        sleep 10
-    done
-
-    # Set the dependency for the next batch
-    dependency=$(IFS:; echo "${batch_jobs[*]}")
-done
