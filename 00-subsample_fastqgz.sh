a0740ed71136e7fe6223d21dbfa2f82251292182
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2023-05-19 00:11:21 +1000

    Update 00-subsample_fastqgz.sh

--- 00-subsample_fastqgz.sh
+++ 00-subsample_fastqgz.sh
@@ -8,12 +8,13 @@
 #SBATCH --time=8:00:00
 #SBATCH --partition=general
 #SBATCH --account=a_crisp
-#SBATCH --requeue 
+#SBATCH --requeue
 
 usage="USAGE:
 sbatch 00-subsample_fastqgz.sh <fastq_directory> <percentage>"
 
-#################################### Setup ########################################
+echo "==================== Setup ===================="
+
 # Check if the number of arguments is correct
 if [ $# -lt 2 ]; then
     echo "$usage"
@@ -29,11 +30,18 @@ path_to_seqtk=/home/s4669612/software/seqtk/seqtk
 path_to_working_directory=$(pwd)
 path_to_raw_reads="$path_to_working_directory"/../raw_reads_template
 
-#################################### Run ####################################
+echo "==================== Run ===================="
+
 # Step 1 - Copy the fastq.gz files from raw_reads_template to the fastq directory
 file_paths=("$path_to_raw_reads"/*)
 file_path=${file_paths[$SLURM_ARRAY_TASK_ID-1]}
 gz_file=$(basename "$file_path")
 
-# Step 2 - Subsample and uncompress the fastq.gz files, store as .fastq - sidenote this took me 26+2 HOURS to make a non-bugged version!!!
+echo "Copying file: $gz_file to $fastq_directory"
+
+# Step 2 - Subsample and uncompress the fastq.gz files, store as .fastq
+echo "Subsampling $gz_file with percentage: $percentage"
 "$path_to_seqtk" sample -s100 "$file_path" "$percentage" > "$fastq_directory"/"${gz_file%.gz}"
+
+echo "Subsampling complete for $gz_file"
+
