ec442021ef6d707b8fd3086da73bb3c1b4db3a7e
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2023-05-19 21:10:01 +1000

    Update 00-pipeline.sh

--- 00-pipeline.sh
+++ 00-pipeline.sh
@@ -57,16 +57,17 @@ if [[ $SLURM_ARRAY_TASK_ID == 2 ]]; then
 fi
 
 #################################### Trim, Bowtie2, and Feature Extraction (with summaries)  ####################################
-echo "Trimming, aligning with Bowtie2, and extracting features..."
-
+echo "Trimming..."
 # Submit the second job and set its dependency on the first job
 cd "$processing_directory"
 trim_galore_job=$(sbatch --parsable --partition=general --dependency=afterok:$fastqc_job "$path_to_trim_script" "$path_to_sample_list" 07:00:00 16 a_crisp "$fastq_directory")
 
+echo Aligning with Bowtie2
 # Submit the third job and set its dependency on the second job, modified bowtie_sbatch to delete the subsampled reads to increase space
 cd "$processing_directory"
 bowtie2_job=$(sbatch --parsable --partition=general --dependency=afterok:$trim_galore_job "$path_to_bowtie_script" "$path_to_sample_list" trimmed 8 "$path_to_reference" 10 09:00:00 50 a_crisp "$fastq_directory")
 
+echo Extracting features
 # Submit the forth job and set its dependency on the third job
 cd "$processing_directory"
 extract_bam_features_job=$(sbatch --parsable --partition=general --dependency=afterok:$bowtie2_job "$path_to_feature_extraction_script" "$processing_directory" "$fastq_directory" "$percentage")
@@ -74,18 +75,15 @@ extract_bam_features_job=$(sbatch --parsable --partition=general --dependency=af
 # Wait until extract_bam_features_job is completed
 while [[ $(squeue -h -j $extract_bam_features_job -t PD,R) ]]; do
     echo sleeping 180
-    sleep 180
+    sleep 240
 done
 
 #################################### Extra 2 - delete processed files to save space ####################################
 echo "Cleaning up..."
-
+echo Removed "$processing_directory" "$fastq_directory" 
 # remove the processing and reads directory (deleted at bowtie sbatch step) - end of the script
 cd "$processing_directory"/..
 rm -r "$processing_directory" "$fastq_directory"
-for file in slurm*; do 
-  rm "$file"
-done
 
 echo "Pipeline completed successfully."
 
