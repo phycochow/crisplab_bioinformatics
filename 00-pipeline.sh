766edeb66c3c4e8b90d376ececf2042e9692febd
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2023-05-19 21:50:48 +1000

    Update 00-pipeline.sh

--- 00-pipeline.sh
+++ 00-pipeline.sh
@@ -57,30 +57,31 @@ if [[ $SLURM_ARRAY_TASK_ID == 2 ]]; then
 fi
 
 #################################### Trim, Bowtie2, and Feature Extraction (with summaries)  ####################################
-echo "Trimming..."
+
 # Submit the second job and set its dependency on the first job
 cd "$processing_directory"
 trim_galore_job=$(sbatch --parsable --partition=general --dependency=afterok:$fastqc_job "$path_to_trim_script" "$path_to_sample_list" 07:00:00 16 a_crisp "$fastq_directory")
+echo Submitted Trimming, id $trim_galore_job
 
-echo Aligning with Bowtie2
 # Submit the third job and set its dependency on the second job, modified bowtie_sbatch to delete the subsampled reads to increase space
 cd "$processing_directory"
 bowtie2_job=$(sbatch --parsable --partition=general --dependency=afterok:$trim_galore_job "$path_to_bowtie_script" "$path_to_sample_list" trimmed 8 "$path_to_reference" 10 09:00:00 50 a_crisp "$fastq_directory")
+echo Submitted Bowtie2, id $bowtie2_job
 
-echo Extracting features
 # Submit the forth job and set its dependency on the third job
 cd "$processing_directory"
 extract_bam_features_job=$(sbatch --parsable --partition=general --dependency=afterok:$bowtie2_job "$path_to_feature_extraction_script" "$processing_directory" "$fastq_directory" "$percentage")
+echo Submitted feature extraction, id $extract_bam_features_job
 
 # Wait until extract_bam_features_job is completed
 while [[ $(squeue -h -j $extract_bam_features_job -t PD,R) ]]; do
-    echo sleeping 180
+    echo "pipeline not completed, sleeping 180"
     sleep 240
 done
 
 #################################### Extra 2 - delete processed files to save space ####################################
 echo "Cleaning up..."
-echo Removed "$processing_directory" "$fastq_directory" 
+echo Removing "$processing_directory" "$fastq_directory" 
 # remove the processing and reads directory (deleted at bowtie sbatch step) - end of the script
 cd "$processing_directory"/..
 rm -r "$processing_directory" "$fastq_directory"
