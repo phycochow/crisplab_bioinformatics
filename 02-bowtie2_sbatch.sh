ae69841fb0446864c14593970eb8276fe867ce16
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2023-06-16 11:36:37 +1000

    Update 02-bowtie2_sbatch.sh

--- 02-bowtie2_sbatch.sh
+++ 02-bowtie2_sbatch.sh
@@ -8,13 +8,12 @@
 #SBATCH --partition=general
 #SBATCH --account=a_crisp
 
-#set -xe
 set -xeuo pipefail
 
 usage="USAGE:
 bash 02-bowtie1_sbatch.sh <sample_list.txt> <reads_folder> <bt2_threads> <bt2_genome.fa> <q10filter> <walltime> <mem> <account_department>"
 
-#define stepo in the pipeline - should be the same name as the script
+#define step in the pipeline - should be the same name as the script
 step=02-bowtie2
 
 #################################### Setup ########################################
@@ -26,7 +25,6 @@ q10filter=$5
 walltime=$6
 mem=$7
 account_department=$8
-fastq_directory=$9
 
 if [ "$#" -lt "8" ]; then
   echo $usage
@@ -34,11 +32,11 @@ if [ "$#" -lt "8" ]; then
 else
   echo "initiating bowtie jobs on $reads_folder folder, bowtie can use $bt2_threads threads"
   cat $sample_list
-  echo genome reference is $bt2_genome
+  echo "genome reference is $bt2_genome"
 fi
 
 # sbatch -J only takes a range to hack - for 1 sample, create a second sample in the input list called "NULL"
-number_of_samples=`wc -l $sample_list | awk '{print $1}'`
+number_of_samples=$(wc -l $sample_list | awk '{print $1}')
 if [[ "$number_of_samples" -eq 1 ]]; then
   sbatch_t=1
 else
@@ -47,68 +45,39 @@ fi
 
 echo "argument to be passed to sbatch -J is '$sbatch_t'"
 
-# #################################### Extra - delete untrimmed fastq files to save space ####################################
-# for file in $fastq_directory; do  done in trim.sh
-#   rm "$file"
-# done
-
-
 #################################### Run ####################################
-
-#make logs directory if it doesnt exist yet - it should
+# Make logs directory if it doesn't exist yet - it should
 mkdir -p logs
 
-#make timestamped trimmgalore logs folder 
+# Make timestamped trimmgalore logs folder 
 timestamp=$(date +%Y%m%d-%H%M%S)
 log_folder=logs/${timestamp}_${step}
 mkdir $log_folder
 
-#script path and cat a record of what was run
+# Script path and cat a record of what was run
 script_dir=~/gitrepos/crisplab_wgs
 script_to_sbatch=${script_dir}/${step}.sh
 cat $script_to_sbatch > ${log_folder}/script.log
 cat $0 > ${log_folder}/sbatch_runner.log
 
-#submit sbatch and pass args
-#-o and -e pass the file locations for std out/error
-#--export additional variables to pass to the sbatch script including the array list and the dir structures
+# Submit sbatch and pass args
+# -o and -e pass the file locations for std out/error
+# --export additional variables to pass to the sbatch script including the array list and the dir structures
 sbatch_output=$(sbatch --array $sbatch_t \
--t $walltime \
--N 1 \
--n 1 \
---cpus-per-task ${bt2_threads} \
---mem-per-cpu ${mem}gb \
--o ${log_folder}/${step}_o_%A_%a \
--e ${log_folder}/${step}_e_%A_%a \
---export LIST=${sample_list},reads_folder=$reads_folder,bt2_threads=$bt2_threads,bt2_genome=$bt2_genome,q10filter=$q10filter \
---account $account_department \
-$script_to_sbatch)
-
+  -t $walltime \
+  -N 1 \
+  -n 1 \
+  --cpus-per-task ${bt2_threads} \
+  --mem-per-cpu ${mem}gb \
+  -o ${log_folder}/${step}_o_%A_%a \
+  -e ${log_folder}/${step}_e_%A_%a \
+  --export LIST=${sample_list},reads_folder=$reads_folder,bt2_threads=$bt2_threads,bt2_genome=$bt2_genome,q10filter=$q10filter \
+  --account $account_department \
+  $script_to_sbatch)
 
 # Extract the job ID from sbatch output
 job_id=$(echo $sbatch_output | awk '{print $4}')
 while [[ $(squeue -h -j $job_id -t PD,R) ]]; do
-    sleep 150
+  sleep 150
 done
 echo "All jobs completed."
-#################################### Keep Running til Completion for Job Dependency ####################################
-
-# # Wait for the sub-sbatch jobs to complete
-# sbatch_exit_status=$(echo $job_id | awk '{print $NF}')
-# if [ "$sbatch_exit_status" != "0" ]; then
-#   echo "Error submitting the sub-sbatch jobs. Exiting."
-#   exit 1
-# fi
-
-# sbatch_job_id=$(echo $job_id | awk '{print $NF}')
-# echo "Submitted sub-sbatch job with ID: $sbatch_job_id"
-
-# # Check the status of the sub-sbatch job and wait until it completes
-# while true; do
-#   job_status=$(squeue -j $sbatch_job_id -h -t PD,R,CG,CA,CF | wc -l)
-#   if [ "$job_status" -eq 0 ]; then
-#     echo "Sub-sbatch job ($sbatch_job_id) completed successfully."
-#   break
-#   fi
-#   sleep 10
-# done
